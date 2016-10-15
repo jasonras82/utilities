e1d90cf92bc24c6335699717ee54c287beb7be98
jasonsanders <jas.sanders@gmail.com>
2016-10-15 13:54:46 -0600

    finished about 20% of problems

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,29 +16,75 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    var newArray = [];
+    if(n === undefined) {
+      return array[0];
+    }
+    else {
+      for(var i = 0; i < n; i++) {
+        if (array[i]) {
+          newArray.push(array[i]);
+        }
+      }
+      return newArray;
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var newArray = [];
+    if(n === undefined) {
+      return array[array.length - 1];
+    }
+    else {
+      for (var i = array.length - 1; i >= array.length - n; i--) {
+        if (array[i]) {
+          newArray.unshift(array[i]);
+        }
+      }
+      return newArray;
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    for(var key in collection) {
+      iterator(collection[key], key, collection)
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    for(var i = 0; i < array.length; i++) {
+      if(array[i] === target) {
+        return i;
+      }
+    } return -1;
   };
 
   // Return all elements of an array that pass a truth test ('iterator' function argument)
   _.filter = function(collection, iterator) {
+      var array = [];
+      for(var i = 0; i < collection.length; i++) {
+        if(iterator(collection[i])) {
+          array.push(collection[i]);
+        }
+      }
+      return array;
   };
 
   // Return all elements of an array that don't pass a truth test (the 'iterator' function argument)
   _.reject = function(collection, iterator) {
+    var array = [];
+    for(var i = 0; i < collection.length; i++) {
+      if(!iterator(collection[i])) {
+        array.push(collection[i]);
+      }
+    }
+    return array;
   };
 
   // Produce a duplicate-free version of the array.
